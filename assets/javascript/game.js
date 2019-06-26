abda297a17f625001755cbac469ae3f52fdf902d
geofell <geofell22@gmail.com>
2019-06-26 14:22:55 -0400

    add javascript to intake a letter, check that the letter is in the alphabet only, ignore duplicates, updated [letters guessed] div, and update the botstrap alert to say game is started

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,18 @@
-document.onkeyup = function(event) {
+var lettersGuessed = [];
+var lettersAllowed = /^[A-Za-z]+$/;
 
-    // Captures the key press, converts it to lowercase, and saves it to a variable.
-    var letter = event.key.toLowerCase();
-    console.log(letter);
-}
+document.onkeyup = function(event) {
+  // Captures the key press, converts it to lowercase, and saves it to a variable.
+  var letter = event.key.toLowerCase();
+  document.getElementById("gameBanner").innerHTML = "Game started.  Good Luck!";
+  document.getElementById("gameBanner").className = "alert alert-warning text-center";
+  console.log(letter);
+  //   if (
+  //     (lettersGuessed.indexOf(letter) === -1 || lettersGuessed.length <= 0) &&
+  //     (typeof letter === "string" || letter instanceof String)
+  //   ) {
+  if (letter.match(lettersAllowed) && letter.length === 1 && lettersGuessed.indexOf(letter) === -1) {
+    lettersGuessed.push(letter);
+    document.getElementById("lettersGuessed").innerHTML = lettersGuessed;
+  }
+};
