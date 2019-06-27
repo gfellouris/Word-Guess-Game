4f1a31c4d23ad591e4745669d2215e909c5a9c2e
geofell <geofell22@gmail.com>
2019-06-26 23:50:41 -0400

    Added basic logic to match word and alert if the word was matched.  Word is hardcoded need to make it a random choice

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,22 +1,34 @@
 var lettersGuessed = [];
 var lettersAllowed = /^[A-Za-z]+$/;
 var guessesRemaining = 12;
+var newGame = true;
+
+var wordPicked = "genesis";
+var wordPickedArray = wordPicked.split("");
+var wordPlaceholderChar = "-";
+var wordPlaceholder = wordPlaceholderChar.repeat(7);
+var wordPlaceholderArray = wordPlaceholder.split("");
 
 document.getElementById("guessesRemaining").innerHTML = guessesRemaining;
+
+// function to initialize game elements
+function initGame() {
+  document.getElementById("gameImage").src = "assets/images/hangman_game.jpg";
+  document.getElementById("gameBanner").innerHTML = "Game started.  Good Luck!";
+  document.getElementById("lettersGuessed").innerHTML = "";
+  document.getElementById("gameBanner").className =
+    "alert alert-warning text-center";
+  newGame = true;
+}
 
 document.onkeyup = function(event) {
   // Captures the key press, converts it to lowercase, and saves it to a variable.
   var letter = event.key.toLowerCase();
-  console.log(letter);
 
-  document.getElementById("gameBanner").innerHTML = "Game started.  Good Luck!";
-  document.getElementById("gameBanner").className =
-    "alert alert-warning text-center";
-
-  //   if (
-  //     (lettersGuessed.indexOf(letter) === -1 || lettersGuessed.length <= 0) &&
-  //     (typeof letter === "string" || letter instanceof String)
-  //   ) {
+  if (newGame) {
+    initGame();
+    newGame = false;
+  }
 
   if (guessesRemaining === 0) {
     document.getElementById("gameBanner").innerHTML =
@@ -24,13 +36,14 @@ document.onkeyup = function(event) {
     document.getElementById("gameBanner").className =
       "alert alert-danger text-center";
     document.getElementById("gameImage").src = "assets/images/loser.jpg";
-    // <img src="assets/images/wordguess-header.jpg" alt="80's Word Guess!" class="mx-auto d-block img-fluid" />
 
     // Initialize variable to start game again
     guessesRemaining = 12;
     lettersGuessed = [];
     alert("Game over!  Sorry too many guesses.");
+    newGame = true;
   } else {
+    // if letter is a-z or A-Z AND its one character long (not tab, caps, etc) AND the letter was typed already
     if (
       letter.match(lettersAllowed) &&
       letter.length === 1 &&
@@ -40,6 +53,30 @@ document.onkeyup = function(event) {
       guessesRemaining--;
       document.getElementById("lettersGuessed").innerHTML = lettersGuessed;
       document.getElementById("guessesRemaining").innerHTML = guessesRemaining;
+      checkGuess(letter);
+      checkWinner(wordPickedArray, wordPlaceholderArray);
     }
   }
 };
+
+function checkWinner(arrCompare1, arrCompare2) {
+  if (arrCompare1.join("") === arrCompare2.join("")) {
+    alert("You won!");
+    document.getElementById("gameImage").src = "assets/images/genesis.jpg";
+    document.getElementById("gameBanner").innerHTML = "You won! Awesome!!!!";
+    // document.getElementById("lettersGuessed").innerHTML = "";
+    document.getElementById("gameBanner").className =
+      "alert alert-success text-center";
+    newGame = true;
+  }
+}
+function checkGuess(str) {
+  for (var i = 0; i < wordPickedArray.length; i++) {
+    if (wordPickedArray[i] === str) {
+      wordPlaceholderArray[i] = str;
+      console.log("wordPlaceholderArray=" + wordPlaceholderArray);
+      wordPlaceholderString = wordPlaceholderArray.join("");
+      document.getElementById("lettersFound").innerHTML = wordPlaceholderString;
+    }
+  }
+}
